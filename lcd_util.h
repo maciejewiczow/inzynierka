bbf21caaf70ef034557683c62604b685a20281b5
Mafciejewicz <melonik0.9846@gmail.com>
2021-10-09 01:38:50 +0200

    Fix namespace errors in utils

--- lcd_util.h
+++ lcd_util.h
@@ -5,29 +5,33 @@
 
 namespace lcdut {
 
-constexpr char deg = 223;
+    namespace symbols {
+        constexpr char deg = 223;
+        constexpr char alpha = 0b11100000;
+        constexpr char beta = 0b11100010;
+    }
 
-struct pos {
-    uint8_t row, col;
+    struct pos {
+        uint8_t row, col;
 
-    explicit pos(uint8_t col, uint8_t row): col(col), row(row) {}
+        explicit pos(uint8_t col, uint8_t row): col(col), row(row) {}
 
-    friend LiquidCrystal_I2C& operator<<(LiquidCrystal_I2C& lcd, const pos& obj) {
-        lcd.setCursor(obj.col, obj.row);
+        friend LiquidCrystal_I2C& operator<<(LiquidCrystal_I2C& lcd, const pos& obj) {
+            lcd.setCursor(obj.col, obj.row);
+            return lcd;
+        }
+    };
+
+    LiquidCrystal_I2C& clear(LiquidCrystal_I2C& lcd) {
+        lcd.clear();
         return lcd;
     }
-};
 
-LiquidCrystal_I2C& clear(LiquidCrystal_I2C& lcd) {
-    lcd.clear();
-    return lcd;
+    LiquidCrystal_I2C& operator<<(LiquidCrystal_I2C& lcd, LiquidCrystal_I2C& (*modifier)(LiquidCrystal_I2C&) ) {
+        modifier(lcd);
+        return lcd;
+    }
 }
 
-}
-
-LiquidCrystal_I2C& operator<<(LiquidCrystal_I2C& lcd, LiquidCrystal_I2C& (*modifier)(LiquidCrystal_I2C&) ) {
-    modifier(lcd);
-    return lcd;
-}
 
 #endif
